08143f1629ae0efd537f90eebaab5b60cf95f41a
graeme <grae22@gmail.com>
2018-10-31 18:51:59 +0000

    Factory method.

---
+++ Patterns/Creational/FactoryMethod/Colour.cs
@@ -0,0 +1,19 @@
+﻿namespace Patterns.Creational.FactoryMethod
+{
+  internal class Colour
+  {
+    public static Colour FromRgb(byte red, byte green, byte blue)
+    {
+      return new Colour(red, green, blue);
+    }
+
+    public static Colour FromName(string name)
+    {
+      return new Colour(0, 0, 0);
+    }
+
+    private Colour(byte red, byte green, byte blue)
+    {
+    }
+  }
+}
---
+++ PatternsTests/Creational/FactoryMethod/ColourTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Patterns.Creational.FactoryMethod;
+
+namespace PatternsTests.Creational.FactoryMethod
+{
+  [TestFixture]
+  public class ColourTests
+  {
+    [Test]
+    public void FromRgb_GivenRgb_ShouldReturnNewInstance()
+    {
+      // Arrange.
+      // Act.
+      Colour result = Colour.FromRgb(255, 128, 64);
+
+      // Assert.
+      Assert.NotNull(result);
+    }
+    
+    [Test]
+    public void FromName_GivenName_ShouldReturnNewInstance()
+    {
+      // Arrange.
+      // Act.
+      Colour result = Colour.FromName("red");
+
+      // Assert.
+      Assert.NotNull(result);
+    }
+  }
+}
