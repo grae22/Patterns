f318c460710d879b3d3c67c3835a722fbd11e622
GraemeB <graeme.bruschi@derivco.co.za>
2018-10-31 13:06:59 +0000

    Builder pattern.

---
+++ Patterns/Creational/Builder/HtmlBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Patterns.Creational.Builder
+{
+  internal class HtmlBuilder
+  {
+    private readonly StringBuilder _resultBuilder = new StringBuilder();
+
+    public string Build()
+    {
+      return _resultBuilder.ToString();
+    }
+
+    public void AddParagraph(string text)
+    {
+      _resultBuilder.Append($"<p>{text}</p>");
+    }
+
+    public void AddBoldText(string text)
+    {
+      _resultBuilder.Append($"<b>{text}</b>");
+    }
+
+    public void AddText(string text)
+    {
+      _resultBuilder.Append(text);
+    }
+  }
+}
---
+++ PatternsTests/Builder/HtmlBuilderTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Patterns.Creational.Builder;
+
+namespace PatternsTests.Builder
+{
+  [TestFixture]
+  public class HtmlBuilderTests
+  {
+    [Test]
+    public void Build_GivenHtmlBuilder_ShouldReturnHtml()
+    {
+      // Arrange.
+      var builder = new HtmlBuilder();
+
+      builder.AddParagraph("Paragraph text 1...");
+      builder.AddBoldText("Bold text");
+      builder.AddText("Plain text");
+      builder.AddParagraph("Paragraph text 2...");
+
+      // Act.
+      string result = builder.Build();
+
+      // Assert.
+      Assert.AreEqual(
+        "<p>Paragraph text 1...</p><b>Bold text</b>Plain text<p>Paragraph text 2...</p>",
+        result);
+    }
+  }
+}
