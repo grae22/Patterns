cc73a5a1a05d3cbea33faf1668b8356ae6973312
GraemeB <graeme.bruschi@derivco.co.za>
2018-11-20 06:37:28 +0000

    Adapter.

---
+++ Patterns/Structural/Adapter/Renderer2d.cs
@@ -0,0 +1,21 @@
+﻿namespace Patterns.Structural.Adapter
+{
+  internal class Renderer2d
+  {
+    public void DrawBox(
+      int x,
+      int y,
+      int width,
+      int height)
+    {
+      var renderer = new Renderer3d();
+
+      renderer.DrawBox(
+        x,
+        y,
+        width,
+        height,
+        0);
+    }
+  }
+}
---
+++ Patterns/Structural/Adapter/Renderer3d.cs
@@ -0,0 +1,14 @@
+﻿namespace Patterns.Structural.Adapter
+{
+  internal class Renderer3d
+  {
+    public void DrawBox(
+      int x,
+      int y,
+      int width,
+      int height,
+      int depth)
+    {
+    }
+  }
+}
